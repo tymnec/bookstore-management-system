56b273b8b05476516633d2fc14be7e600ba34c61
Nikhil Sarwara <tymnec@gmail.com>
2023-06-27 13:13:38 +0530

    declare deletebook func

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -5,11 +5,12 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 	"github.com/tymnec/go-bookstore/pkg/models"
 	"github.com/tymnec/go-bookstore/pkg/utils"
-	"net/http"
-	"strconv"
 )
 
 var NewBook models.Book
@@ -43,6 +44,20 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	utils.ParseBody(r, CreateBook)
 	b := CreateBook.CreateBook()
 	res, _ := json.Marshal(b)
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("err while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
