3d60057bb16ff1194ab54be4221b0d427986b6d4
Nikhil Sarwara <tymnec@gmail.com>
2023-06-27 12:18:11 +0530

    define book struct

--- go-bookstore/pkg/models/book.go
+++ go-bookstore/pkg/models/book.go
@@ -6,3 +6,12 @@ import (
 	"github.com/jinzhu/gorm"
 	"github.com/tymnec/go-bookstore/pkg/config"
 )
+
+var db *gorm.DB
+
+type Book struct{
+	gorm.Model
+	Name string `gorm:""json:"name"`
+	Author string `json:"author"`
+	Publication string `json:"publication"`
+}
