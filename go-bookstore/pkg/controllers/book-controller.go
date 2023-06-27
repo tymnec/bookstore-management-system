ba3889a12e8238a5eb79f9080aad9200b74867d4
Nikhil Sarwara <tymnec@gmail.com>
2023-06-27 12:37:50 +0530

    define getbook func in controllers

--- go-bookstore/pkg/controllers/book-controller.go
+++ go-bookstore/pkg/controllers/book-controller.go
@@ -1,4 +1,4 @@
-// Controllers 
+// Controllers
 
 package controllers
 
@@ -6,15 +6,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/tymnec/go-bookstore/pkg/models"
+	"github.com/tymnec/go-bookstore/pkg/utils"
 	"net/http"
 	"strconv"
-	"github.com/tymnec/go-bookstore/pkg/utils"
-	"github.com/tymnec/go-bookstore/pkg/models"
 )
 
 var NewBook models.Book
 
-func GetBook(w http.ResponseWriter, r *http.Request){
+func GetBook(w http.ResponseWriter, r *http.Request) {
 	newBooks := models.GetAllBooks()
-	
+	res, _ := json.Marshal(newBooks)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
