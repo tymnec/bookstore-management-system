181097695a5055836edbfe4434e38b33e552a5cf
Nikhil Sarwara <tymnec@gmail.com>
2023-06-27 12:00:28 +0530

    add getbook func

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -9,5 +9,6 @@ import (
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
-	router.HandleFunc("/book/", controllers.)
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	
 }
