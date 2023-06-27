f5d117bfeea084646ec98dd812c24c122a50865e
Nikhil Sarwara <tymnec@gmail.com>
2023-06-27 12:05:15 +0530

    add deletebook func

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -12,5 +12,5 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
 	router.HandleFunc("/book/{bookid}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/book/{bookid}", controllers.UpdateBook).Methods("PUT")
-	
+	router.HandleFunc("/book/{bookid}", controllers.DeleteBook).Methods("DELETE")
 }
