75aa3b939aa9ae1b6b37c30c828788bb85cf3fb3
Nikhil Sarwara <tymnec@gmail.com>
2023-06-27 12:14:40 +0530

    complete parse body function

--- go-bookstore/pkg/utils/utils.go
+++ go-bookstore/pkg/utils/utils.go
@@ -2,6 +2,15 @@ package utils
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
+
+// Parse Body Function
+func ParseBody(r *http.Request, x interface{}) {
+	if body, err := io.ReadAll(r.Body); err == nil{
+		if err := json.Unmarshal([]byte(body), x); err != nil{
+			return
+		}
+	}
+}
